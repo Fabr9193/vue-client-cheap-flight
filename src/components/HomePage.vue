cba779394e514b65a7ba057c1962d6e777398b9b
Fabricio Alegria <fabr9193@gmail.com>
2019-12-30 22:21:37 +0100

    adding axios and start connection

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -10,17 +10,17 @@
                             <label for="fly_from">Départ de:  </label>
                            </div>
                            <div class="ml-0 md:ml-2 w-full md:w-3/4">
-                            <input  class="w-full"  v-model="userData.departure" id="fly_from" type="text">
+                            <input  class="w-full"  v-model="userData.fly_from" id="fly_from" type="text">
                            </div>
                        </div>
                    <div class="w-full flex flex-wrap justify-center">
 
                        <div class="w-1/2 flex flex-col md:flex-row ">
                                <div>
-                                   <label for="budget">Budget</label>
+                                   <label for="price">price</label>
                                </div>
                                <div class="ml-0 md:ml-2">
-                                   <input id="budget" type="number" v-model.number="userData.budget">&nbsp;€
+                                   <input id="price" type="number" v-model.number="userData.price">&nbsp;€
                                </div>
                            </div>
                        <div class="flex flex-col md:flex-row justify-center align-middle">
@@ -28,7 +28,7 @@
                                <label for="nb_travelers">Voyageurs : </label>
                            </div>
                            <div class="ml-0 md:ml-2">
-                               <input id="nb_travelers" v-model.number="userData.nbTravelers" type="number">
+                               <input id="nb_travelers" v-model.number="userData.adults" type="number">
                            </div>
                        </div>
                    </div>
@@ -63,32 +63,36 @@
                     </div>
                 </div>
             </div>
-               <button type="submit" v-on:click="checkForm" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button>
+               <button type="submit" v-on:click="checkForm()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button>
            </div>
   </div>
 </template>
 
 <script>
   //  const urlApi = 'http://google.com';
+  const axios =require("axios")
 export default {
   name: 'HomePage',
     data(){
       return {
           userData : {
           regions: [],
-          nbTravelers: 0,
-          budget: 0,
-          departure: '',
+          adults: 0,
+          price: 0,
+          fly_from: '',
         }}
       },
     computed: {
 
     },
     methods: {
-      checkForm: (element) => {
-          console.log(element);
-        console.log(this.nbTravelers);
-        console.log(this.budget);
+      checkForm: function () {
+          console.log();
+        axios.get(' https://fast-sierra-13727.herokuapp.com/search?' , this.userData).then(function (result){
+            console.log('here')
+            console.log(result);
+        })
+
       },
 
     }
